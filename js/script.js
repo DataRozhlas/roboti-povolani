1fff13cc28174b08c06469177cc62a69c9150195
timichal <michal@zlatkovsky.cz>
2018-10-29 17:18:39 +0100

    grafy

--- js/script.js
+++ js/script.js
@@ -46,9 +46,11 @@ $( function () {
     }
 
     function loadData(job) {
-        var slug = bezdiak(job).toLowerCase().replace(/ /g,"_").replace(/,|"/g,"");
+        var slug = bezdiak(job).toLowerCase().replace(/ /g,"_").replace(/"/g,"");
         $.getJSON("https://data.irozhlas.cz/roboti-povolani/jobs_data/" + slug + ".json", function(data) {
             drawChartGeneral(data.obecne);
+            drawChartSoft(data.mekke);
+            drawRoboBar(data);
         });
     }
 
@@ -64,16 +66,16 @@ $( function () {
                 values.push(0);
             }
         })
-        console.log(data);
+
         var chart = Highcharts.chart('graf1', {
 
             chart: {
                 polar: true,
-                type: 'line'
+                type: 'area'
             },
 
             title: {
-                text: 'Budget vs spending',
+                text: 'Obecné dovednosti',
             },
 
             pane: {
@@ -89,13 +91,17 @@ $( function () {
             yAxis: {
                 gridLineInterpolation: 'polygon',
                 lineWidth: 0,
+                tickInterval: 1,
                 min: 0,
-                max: 3
+                max: 3,
+                labels: {
+                    enabled: false
+                }
             },
 
             tooltip: {
                 shared: true,
-                pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
+                pointFormat: '<span style="color:{series.color}">{series.name}: <b>{point.y:,.0f}</b><br/>'
             },
 
             legend: {
@@ -106,13 +112,215 @@ $( function () {
                 enabled: false
             },
 
+            credits: {
+                enabled: false
+            },
+
             series: [{
-                name: 'Allocated Budget',
+                name: 'Úroveň kvalifikace (1-3)',
                 data: values,
                 pointPlacement: 'on'
-            }]
+            }],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        pane: {
+                            size: '50%'
+                        },
+                    }
+                }]
+            }
 
         });
+    }
+
+    function drawChartSoft(data) {
+        var categories = ["Celoživotní učení", "Zvládání zátěže", "Samostatnost", "Objevování a orientace v informacích", 
+        "Kooperace (spolupráce)", "Uspokojování zákaznických potřeb", "Kreativita",
+        "Aktivní přístup", "Flexibilita", "Plánování a organizování práce", 
+        "Vedení lidí (leadership)", "Výkonnost", "Ovlivňování ostatních", 
+        "Efektivní komunikace", "Řešení problémů"];
+        var values = [];
+        categories.forEach(function(category) {
+            if (data[category]) {
+                values.push(data[category]);
+            } else {
+                values.push(0);
+            }
+        })
+        var chart = Highcharts.chart('graf2', {
+
+            chart: {
+                polar: true,
+                type: 'area'
+            },
+
+            title: {
+                text: 'Měkké kompetence',
+            },
+
+            pane: {
+                size: '80%'
+            },
+
+            xAxis: {
+                categories: categories,
+                tickmarkPlacement: 'on',
+                lineWidth: 0
+            },
+
+            yAxis: {
+                gridLineInterpolation: 'polygon',
+                lineWidth: 0,
+                tickInterval: 1,
+                min: 0,
+                max: 5,
+                labels: {
+                    enabled: false
+                }
+            },
+
+            tooltip: {
+                shared: true,
+                pointFormat: '<span style="color:{series.color}">{series.name}: <b>{point.y:,.0f}</b><br/>'
+            },
+
+            legend: {
+                enabled: false
+            },
+
+            credits: {
+                enabled: false
+            },
+
+            series: [{
+                name: 'Úroveň kvalifikace (1-5)',
+                data: values,
+                pointPlacement: 'on'
+            }],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        pane: {
+                            size: '65%'
+                        },
+                        xAxis: {
+                            labels: {
+                                style: {
+                                    "fontSize": "8px"
+                                }
+                            }
+                        },
+                    }
+                }]
+            }
+
+        });
+    }
+
+    function drawRoboBar(data) {
+        var robotized = ["Počítačová způsobilost", "Numerická způsobilost", "Jazyková způsobilost v dalším cizím jazyce", 
+                "Jazyková způsobilost v angličtině", "Způsobilost k řízení osobního automobilu", "Celoživotní učení",
+                "Zvládání zátěže", "Kooperace (spolupráce)", "Flexibilita", "Plánování a organizování práce", 
+                "Výkonnost"];
+        var catGeneral = ["Počítačová způsobilost", "Numerická způsobilost", "Jazyková způsobilost v dalším cizím jazyce", 
+                "Jazyková způsobilost v češtině", "Ekonomické povědomí", "Právní povědomí", "Jazyková způsobilost v angličtině",
+                "Způsobilost k řízení osobního automobilu"];
+        var catSoft = ["Celoživotní učení", "Zvládání zátěže", "Samostatnost", "Objevování a orientace v informacích", 
+        "Kooperace (spolupráce)", "Uspokojování zákaznických potřeb", "Kreativita",
+        "Aktivní přístup", "Flexibilita", "Plánování a organizování práce", 
+        "Vedení lidí (leadership)", "Výkonnost", "Ovlivňování ostatních", 
+        "Efektivní komunikace", "Řešení problémů"];
+        var values = {};
+        for (var attrname in data.obecne) { values[attrname] = data.obecne[attrname]; }
+        for (var attrname in data.mekke) { values[attrname] = data.mekke[attrname]; }
+
+        // total: sum of values for each competence, partial: sum of values computerized 
+
+        var genTotal = 0;
+        var genRobo = 0;
+        var affected = [];
+
+        catGeneral.forEach(function(category) {
+            if (data.obecne[category]) {
+                genTotal += data.obecne[category];
+                if ($.inArray(category, robotized) !== -1) {
+                    genRobo += data.obecne[category];
+                    affected.push(category);
+                }
+            }
+        });
+        
+        catSoft.forEach(function(category) {
+            if (data.mekke[category]) {
+                genTotal += data.mekke[category]/5*3;
+                if ($.inArray(category, robotized) !== -1) {
+                    genRobo += data.mekke[category]/5*3;
+                    affected.push(category);
+                }
+            }
+        });
+        
+        genTotal = parseInt(genTotal);
+        genRobo = parseInt(genRobo);
+
+        var ratio = parseInt(genRobo/genTotal*100)
+        console.log(ratio);
+
+        var chart = Highcharts.chart('grafBar', {
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Roboti vás zastoupí v ' + ratio + " % kompetencí"
+            },
+            xAxis: {
+                categories: ['Robotizace'],
+                labels: {
+                    enabled: false
+                },
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    enabled: false
+                }
+            },
+            tooltip: {
+                enabled: false
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                series: {
+                    stacking: 'percent'
+                }
+            },
+            series: [{
+                name: 'Nerobotizováno',
+                data: [genTotal-genRobo]
+            },{
+                name: 'Robotizováno',
+                data: [genRobo]
+            }],
+            credits: {
+                enabled: false
+            }
+        });
+        affected_str = "";
+        affected.forEach(function(competence) {
+            affected_str += competence.toLowerCase() + ", "
+        });
+        $("#result").text("Roboti vás zastoupí v těchto oblastech: " + affected_str.replace(/, $/,"."));
     }
 
     function bezdiak(text) {
